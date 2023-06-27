753ebe8ec373364f03273a29926a1d0a98e522ec
greensgit <rajgreensomr@gmail.com>
2023-06-27 12:39:25 +0530

    new code added by santhosh

--- src/org/sde/test/Test.java
+++ src/org/sde/test/Test.java
@@ -5,5 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test project");
 	}
+	
+	private void login() {
+		System.out.println("This is santhosh working in login");
+	}
 
 }
